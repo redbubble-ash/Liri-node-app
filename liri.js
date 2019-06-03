207b496569d22678629c475b70bc0e9119bd0515
Your Name <you@example.com>
2019-06-02 23:19:56 -0700

    added concert-this through The Bands in Town

--- liri.js
+++ liri.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 
-var axios = require("axios");
+const axios = require("axios");
 
 var task = process.argv[2];
 
@@ -63,10 +63,63 @@ if (task === "spotify-this-song") {
                 console.log(err);
             });
 
-
-
-
     }
 }
 
 
+//The Bands in Town
+
+if (task === "concert-this"){
+
+    var artist = process.argv;
+
+    var artistName = "";
+
+    for (var i = 3; i < artist.length; i++) {
+
+        if (i > 3 && i < artist.length) {
+          artistName = artistName + "+" + artist[i];
+        } else {
+          artistName += artist[i];
+      
+        }
+      }
+
+ var queryUrl = "https://rest.bandsintown.com/artists/" + artistName + "/events?app_id=codingbootcamp"
+      
+ axios.get(queryUrl).then(
+    function(response) {
+      console.log("==============================");
+      console.log(response.data[0].lineup[0] +" is playing at:");
+      console.log("Name of the venue: " + response.data[0].venue.name);
+      console.log("Venue location: " + response.data[0].venue.city + ", " + response.data[0].venue.region);
+      console.log("Date of the Event: " + response.data[0].datetime);
+
+
+
+    })
+    .catch(function(error) {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log("---------------Data---------------");
+        console.log(error.response.data);
+        console.log("---------------Status---------------");
+        console.log(error.response.status);
+        console.log("---------------Status---------------");
+        console.log(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an object that comes back with details pertaining to the error that occurred.
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log("Error", error.message);
+      }
+      console.log(error.config);
+    });
+  
+
+
+
+}
