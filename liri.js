9b7771330af5e34408561920bbe5f66f2f8868a6
Your Name <you@example.com>
2019-06-02 21:37:44 -0700

    fixed bug to show spotify info for the song with one word

--- liri.js
+++ liri.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 var axios = require("axios");
 
+// Spotify:
 // Add the code required to import the `keys.js` file and store it in a variable.
 var keys = require("./keys.js");
 
@@ -11,13 +12,9 @@ var spotify = new Spotify(keys.spotify);
 
 var task = process.argv[2];
 
-// var song = process.argv[2];
-
-// console.log(song);
-
 if (task === "spotify-this-song") {
 
-    var song = process.argv.slice(1);
+    var song = process.argv;
     var songName = "";
 
     for (var i = 3; i < song.length; i++) {
@@ -30,15 +27,20 @@ if (task === "spotify-this-song") {
       
         }
       }
-    
 
     spotify
         .search({ type: 'track', query: songName })
         .then(function (response) {
+            console.log("==============================");
+            console.log("Spotify Results for "+JSON.stringify(response.tracks.items[0].name));
+            console.log("\n");
             console.log("Artist(s) Name: "+JSON.stringify(response.tracks.items[0].artists[0].name));
             console.log("The song's name: "+JSON.stringify(response.tracks.items[0].name));
             console.log("A preview link of the song from Spotify: "+JSON.stringify(response.tracks.items[0].external_urls.spotify));
             console.log("The album that the song is from: "+JSON.stringify(response.tracks.items[0].album.name));
+            console.log("\n");
+            console.log("==============================");
+
         })
         .catch(function (err) {
             console.log(err);
